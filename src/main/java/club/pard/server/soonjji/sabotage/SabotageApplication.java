557dd074328bf4bcf673fe2e256fe9efa7a94111
Turbstructor <ycw0201@naver.com>
2023-12-27 17:50:20 +0900

    Initial commit: Initializr byproduct

---
+++ src/main/java/club/pard/server/soonjji/sabotage/SabotageApplication.java
@@ -0,0 +1,13 @@
+package club.pard.server.soonjji.sabotage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabotageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabotageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/pard/server/soonjji/sabotage/SabotageApplicationTests.java
@@ -0,0 +1,13 @@
+package club.pard.server.soonjji.sabotage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabotageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
