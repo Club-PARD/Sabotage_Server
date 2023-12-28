ceb7b3521d42dc5fb74f729727594e47c5dea9ff
Turbstructor <ycw0201@naver.com>
2023-12-28 22:13:20 +0900

    Fix: no more blank page

--- src/main/java/club/pard/server/soonjji/sabotage/configuration/WebSecurityConfig.java
+++ src/main/java/club/pard/server/soonjji/sabotage/configuration/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig {
             라인별 해석:
             1. cors정책 (현재는 Application에서 작업을 해뒀으므로 기본 설정 사용)
             2. csrf 대책 (현재는 CSRF에 대한 대책을 비활성화)
-            // 3. basic 인증 (현재는 bearer token 인증방법을 사용하기 때문에 비활성화)
+            3. basic 인증 (현재는 bearer token 인증방법을 사용하기 때문에 비활성화)
             4. 세션 기반 인증 (현재는 Session 기반 인증을 사용하지 않기 때문에 상태를 없앰)
             5. "/" "/api/auth" 모듈에 대해서는 모두 허용 (인증을 하지 않고 사용 가능하게 함)
             6. 나머지 REquest에 대해서는 모두 인증된 사용자만 사용가능하게함
@@ -28,9 +28,9 @@ public class WebSecurityConfig {
         httpSecurity
                 .cors(Customizer.withDefaults())
                 .csrf((csrf) -> csrf.disable())
-                // .httpBasic((httpBasic) -> httpBasic.disable())
+                .httpBasic((httpBasic) -> httpBasic.disable())
                 .sessionManagement((sessionManagement) ->sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers("/swagger-ui/index.html")
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**")
                 .permitAll().anyRequest().authenticated());
 
         return httpSecurity.build();
