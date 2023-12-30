5d4869704c6b645f4eba25a825fe868577b74b04
Turbstructor <ycw0201@naver.com>
2023-12-30 15:30:47 +0900

    Feat: add comment on Response

--- src/main/java/club/pard/server/soonjji/sabotage/dto/Response/Response.java
+++ src/main/java/club/pard/server/soonjji/sabotage/dto/Response/Response.java
@@ -6,10 +6,11 @@ import lombok.Data;
 @Data
 @AllArgsConstructor(staticName = "set")
 public class Response<D> {
-    private boolean result;
-    private String message;
-    private D data;
+    private boolean isSuccessful;   // Basic boolean value for call result
+    private String message;         // For client developers
+    private String comment;         // For server developers
+    private D data;                 // What client will receive after request
 
-    public static <D> Response<D> setSuccess(String message, D data){ return Response.set(true, message, data); }
-    public static <D> Response<D> setFailure(String message){ return Response.set(false, message, null); }
+    public static <D> Response<D> setSuccess(String message, String comment, D data){ return Response.set(true,     message, comment, data); }
+    public static <D> Response<D> setFailure(String message, String comment)        { return Response.set(false,    message, comment, null); }
 }
