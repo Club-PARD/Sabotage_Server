12dfc9c356eb015b1a699c8fdbbdbb849c90d7a6
Turbstructor <ycw0201@naver.com>
2024-01-02 13:56:12 +0900

    Fix: moved GlobalExceptionHandler

--- src/main/java/club/pard/server/soonjji/sabotage/exception/GlobalExceptionHandler.java
+++ src/main/java/club/pard/server/soonjji/sabotage/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
-package club.pard.server.soonjji.exception;
+package club.pard.server.soonjji.sabotage.exception;
+
+import java.util.StringJoiner;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,15 +16,16 @@ import club.pard.server.soonjji.sabotage.dto.response.Response;
 public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Response<?> handleValidationException(MethodArgumentNotValidException e)
+    public ResponseEntity<Response<?>> handleValidationException(MethodArgumentNotValidException e)
     {
-        StringBuilder allErrors = new StringBuilder();
+        StringJoiner allErrors = new StringJoiner(" / ");
         e.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
-            allErrors.append(String.format("%s: %s / ", fieldName, errorMessage));
+            allErrors.add(String.format("%s: %s", fieldName, errorMessage));
         });
 
-        return Response.setFailure("올바르지 않은 값이 들어왔어요!", allErrors.toString());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(Response.setFailure("올바르지 않은 값이 들어왔어요!", allErrors.toString()));
     }
 }
